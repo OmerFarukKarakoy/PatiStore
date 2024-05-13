dae9d71ac901fc5099baef687054ecfe03e3eb09
OmerFarukKarakoy <156546037+OmerFarukKarakoy@users.noreply.github.com>
2024-05-13 19:01:24 +0300

    Product.js

--- Product.js
+++ Product.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {View, Text, Image, Price} from 'react-native';
 import styles from './Product.style';
 
-const NewsCard = ({news}) => {
+const Product = ({product}) => {
     return(
         <View style={styles.container}>
-            <Image style={styles.image} source={{uri:news.imageUrl}} />
+            <Image style={styles.image} source={{uri:product.imageUrl}} />
             <View style={styles.inner_container}>
-                <Text style={styles.title}>{news.title}</Text>
-                <Text style={styles.price}>{news.price}</Text>
-                {!news.inStock && <Text style={styles.outOfStock}>STOKTA YOK</Text>}
+                <Text style={styles.title}>{product.title}</Text>
+                <Text style={styles.price}>{product.price}</Text>
+                {!product.inStock && <Text style={styles.outOfStock}>STOKTA YOK</Text>}
             </View>
         </View>
     );
 };
 
-export default NewsCard;
+export default Product;
